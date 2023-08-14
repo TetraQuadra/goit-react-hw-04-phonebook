c689b935ac8312bee0d70cf71539ac7d62beaa00
TetraQuadra <122919279+TetraQuadra@users.noreply.github.com>
2023-08-14 04:58:22 +0300

    fix: removed unused func

--- src/components/ContactForm/ContactForm.jsx
+++ src/components/ContactForm/ContactForm.jsx
@@ -2,23 +2,23 @@ import React, { useState } from 'react';
 import PropTypes from 'prop-types';
 
 const ContactForm = ({ handleSubmit }) => {
-  const [name, setName] = useState('');
-  const [number, setNumber] = useState('');
+  const [contact, setContact] = useState({
+    name: '',
+    number: '',
+  });
 
   const handleChange = (e) => {
     const { name, value } = e.target;
-    if (name === 'name') {
-      setName(value);
-    } else if (name === 'number') {
-      setNumber(value);
-    }
+    setContact((prevContact) => ({
+      ...prevContact,
+      [name]: value,
+    }));
   };
 
   const onSubmit = (e) => {
     e.preventDefault();
-    handleSubmit({ name, number });
-    setName('');
-    setNumber('');
+    handleSubmit(contact);
+    setContact({ name: '', number: '' });
   };
 
   return (
@@ -29,7 +29,7 @@ const ContactForm = ({ handleSubmit }) => {
         pattern="^[a-zA-Zа-яА-Я]+(([' \-][a-zA-Zа-яА-Я ])?[a-zA-Zа-яА-Я]*)*$"
         title="Name may contain only letters, apostrophe, dash and spaces. For example Adrian, Jacob Mercer, Charles de Batz de Castelmore d'Artagnan"
         required
-        value={name}
+        value={contact.name}
         onChange={handleChange}
         placeholder="Name"
       />
@@ -39,7 +39,7 @@ const ContactForm = ({ handleSubmit }) => {
         pattern="\+?\d{1,4}?[ .\-\s]?\(?\d{1,3}?\)?[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,4}[ .\-\s]?\d{1,9}"
         title="Phone number must be digits and can contain spaces, dashes, parentheses and can start with +"
         required
-        value={number}
+        value={contact.number}
         onChange={handleChange}
         placeholder="Phone Number"
       />
